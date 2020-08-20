d15e7f3fa70421e9974784b881f8d63c8a8d048e
Saul Maldonado <samaldonado4@gmail.com>
2020-08-20 07:03:27 -0500

    Add rate limited and bad word filter
    Add rate-limiter-flexible package. Rate limit incoming messages
    according to the value in .env. Add badword filter to prevent emitting
    offensive words.
    
    Add dotenv package
    Add .gitsecret for safely storing secret files

--- index.ts
+++ index.ts
@@ -2,20 +2,50 @@ import koa from 'koa';
 import { createServer } from 'http';
 import socketio, { Socket } from 'socket.io';
 import koaStatic from 'koa-static';
+import { RateLimiterMemory } from 'rate-limiter-flexible';
+import { config } from 'dotenv';
 
 import events from './socketIoEvents';
+import badWords from './badwords.json';
+
+config();
 
 const app = new koa();
 
 const server = createServer(app.callback());
 const io = socketio(server);
 
+// rate limiter
+const rateLimiter = new RateLimiterMemory({
+  points: Number(process.env.POINTS_PER_SECOND!),
+  duration: 1,
+});
+
 app.use(koaStatic(__dirname));
 
-io.on('connection', (socket: Socket) => {
+// helper methods
+const rateLimit = async (socket: Socket) => {
+  try {
+    await rateLimiter.consume(socket.handshake.address);
+  } catch (rejRes) {
+    socket.emit('blocked', { retry: rejRes.msBeforeNext });
+  }
+};
+
+const filterMessage = async (message: string, socket: Socket) => {
+  if (!badWords.includes(message)) {
+    return socket.emit(events.SEND_MESSAGE, message);
+  }
+
+  return socket.emit('BAD_WORD', "you can't say that");
+};
+
+// io
+io.on('connection', async (socket: Socket) => {
   console.log('a new user connected');
-  socket.on(events.SEND_MESSAGE, (message: string) => {
-    socket.broadcast.emit(message);
+  socket.on(events.SEND_MESSAGE, async (message: string) => {
+    await rateLimit(socket);
+    await filterMessage(message, socket);
   });
 });
 
